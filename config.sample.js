1e62aad4a9f14d66e3ec403f3bea096175441cb2
LOUIZA BEATRICE LEGASPI LIM <20230027697@my.xu.edu.ph>
2026-09-08 10:57:29 +0800

    Delete config.sample.js

--- config.sample.js
+++
@@ -1,3 +0,0 @@
-// Copy this file to config.js and replace the placeholder with your Pixabay API key.
-// Get a key at: https://pixabay.com/api/docs/
-window.PIXABAY_API_KEY = "YOUR_PIXABAY_API_KEY_HERE";
